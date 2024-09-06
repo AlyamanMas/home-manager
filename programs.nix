45b6cbf19357e4cf2d6c6c9b52903d3960327366
Alyaman Massarani <alyaman.maasarani@gmail.com>
2024-09-05 23:43:52 -0400

    Separated fish's config into new dir in ./programs/

--- programs.nix
+++ programs.nix
@@ -3,7 +3,7 @@
   pkgs,
   inputs,
   ...
-}:
+}@attrs:
 
 {
   home.packages = with pkgs; [
@@ -61,26 +61,5 @@
     enable = true;
   };
 
-  programs.fish = {
-    enable = true;
-    plugins = [
-      {
-        # Allows to source bash/shell scripts
-        name = "bass";
-        src = pkgs.fetchFromGitHub {
-          owner = "edc";
-          repo = "bass";
-          rev = "79b62958ecf4e87334f24d6743e5766475bcf4d0";
-          sha256 = "0dy53vzzpclw811gxv1kazb8rm7r9dyx56f5ahwd1g38x0pympyx";
-        };
-      }
-      {
-        name = "plugin-git";
-        src = pkgs.fishPlugins.plugin-git.src;
-      }
-    ];
-    shellInit = ''
-      set -gx EDITOR nvim
-    '';
-  };
+  programs.fish = import ./programs/fish.nix attrs;
 }
---
+++ programs/fish.nix
@@ -0,0 +1,29 @@
+{
+  config,
+  pkgs,
+  inputs,
+  ...
+}:
+
+{
+  enable = true;
+  plugins = [
+    {
+      # Allows to source bash/shell scripts
+      name = "bass";
+      src = pkgs.fetchFromGitHub {
+        owner = "edc";
+        repo = "bass";
+        rev = "79b62958ecf4e87334f24d6743e5766475bcf4d0";
+        sha256 = "0dy53vzzpclw811gxv1kazb8rm7r9dyx56f5ahwd1g38x0pympyx";
+      };
+    }
+    {
+      name = "plugin-git";
+      inherit (pkgs.fishPlugins.plugin-git) src;
+    }
+  ];
+  shellInit = ''
+    set -gx EDITOR nvim
+  '';
+}
