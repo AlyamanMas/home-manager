cc87a717a0683123f4a820a3bd79eb2e2a255493
Alyaman Massarani <alyaman.maasarani@gmail.com>
2024-10-31 16:12:47 -0400

    Changed nixvim theme to catppuccin

--- nixvim/config/default.nix
+++ nixvim/config/default.nix
@@ -87,6 +87,12 @@
     };
   };
   config = {
+    colorschemes.catppuccin = {
+      enable = true;
+      settings = {
+        transparent_background = true;
+      };
+    };
     # The base16 theme to use, if you want to use another theme, change it in colorscheme.nix
     theme = "paradise";
     extraConfigLua = ''
