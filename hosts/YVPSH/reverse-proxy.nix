ad0aee2e995ed1a6e478c47916235f5af80168a4
Alyaman Massarani <alyaman.maasarani@gmail.com>
2025-06-08 13:33:36 +0300

    added reverse-proxy entry for timer.aucsymposium.com

--- hosts/YVPSH/reverse-proxy.nix
+++ hosts/YVPSH/reverse-proxy.nix
@@ -103,7 +103,22 @@
               "proxy_pass_header Authorization;";
         };
       };
-    };
+      "timer.aucsymposium.com" = {
+        sslCertificate = "/etc/ssl/certs/cf.crt";
+        sslCertificateKey = "/etc/ssl/private/cf.key";
+        forceSSL = true;
+        locations."/" = {
+          proxyPass = "http://127.0.0.1:3391";
+          proxyWebsockets = true; # needed if you need to use WebSocket
+          extraConfig =
+            # required when the target is also TLS server with multiple hosts
+            "proxy_ssl_server_name on;"
+            +
+              # required when the server wants to use HTTP Authentication
+              "proxy_pass_header Authorization;";
+        };
+      };
+    }; # end virtualHosts
   };
   security.acme = {
     acceptTerms = true;
