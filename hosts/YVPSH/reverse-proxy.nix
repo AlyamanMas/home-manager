d589086d48e540c4384a4b6bb36c7f0d3b113bc5
Alyaman Massarani <alyaman.maasarani@gmail.com>
2025-06-19 01:14:46 +0300

    Changed timer.aucsymposium.com to point to a static dir

--- hosts/YVPSH/reverse-proxy.nix
+++ hosts/YVPSH/reverse-proxy.nix
@@ -108,14 +108,7 @@
         sslCertificateKey = "/etc/ssl/private/cf.key";
         forceSSL = true;
         locations."/" = {
-          proxyPass = "http://127.0.0.1:3391";
-          proxyWebsockets = true; # needed if you need to use WebSocket
-          extraConfig =
-            # required when the target is also TLS server with multiple hosts
-            "proxy_ssl_server_name on;"
-            +
-              # required when the server wants to use HTTP Authentication
-              "proxy_pass_header Authorization;";
+          root = "/www/aucsymposium/timer/";
         };
       };
     }; # end virtualHosts
