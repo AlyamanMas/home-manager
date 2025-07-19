837042295772f2df73fb647bc92d1d7df31ad18f
Alyaman Massarani <alyaman.maasarani@gmail.com>
2025-07-19 21:32:41 +0300

    Disabled searxng and cleaned up proxy file

--- hosts/YVPSH/reverse-proxy.nix
+++ hosts/YVPSH/reverse-proxy.nix
@@ -43,51 +43,51 @@
               "proxy_pass_header Authorization;";
         };
       };
-      "wui.tlsymposium.com" = {
-        sslCertificate = "/etc/ssl/certs/cf.crt";
-        sslCertificateKey = "/etc/ssl/private/cf.key";
-        forceSSL = true;
-        locations."/" = {
-          proxyPass = "http://127.0.0.1:1115";
-          proxyWebsockets = true; # needed if you need to use WebSocket
-          extraConfig =
-            # required when the target is also TLS server with multiple hosts
-            "proxy_ssl_server_name on;"
-            +
-              # required when the server wants to use HTTP Authentication
-              "proxy_pass_header Authorization;";
-        };
-      };
-      "jp.tlsymposium.com" = {
-        sslCertificate = "/etc/ssl/certs/cf.crt";
-        sslCertificateKey = "/etc/ssl/private/cf.key";
-        forceSSL = true;
-        locations."/" = {
-          proxyPass = "http://127.0.0.1:10010";
-          proxyWebsockets = true; # needed if you need to use WebSocket
-          extraConfig =
-            # required when the target is also TLS server with multiple hosts
-            "proxy_ssl_server_name on;"
-            +
-              # required when the server wants to use HTTP Authentication
-              "proxy_pass_header Authorization;";
-        };
-      };
-      "sxng.tlsymposium.com" = {
-        sslCertificate = "/etc/ssl/certs/cf.crt";
-        sslCertificateKey = "/etc/ssl/private/cf.key";
-        forceSSL = true;
-        locations."/" = {
-          proxyPass = "http://127.0.0.1:8787";
-          proxyWebsockets = true; # needed if you need to use WebSocket
-          extraConfig =
-            # required when the target is also TLS server with multiple hosts
-            "proxy_ssl_server_name on;"
-            +
-              # required when the server wants to use HTTP Authentication
-              "proxy_pass_header Authorization;";
-        };
-      };
+      # "wui.tlsymposium.com" = {
+      #   sslCertificate = "/etc/ssl/certs/cf.crt";
+      #   sslCertificateKey = "/etc/ssl/private/cf.key";
+      #   forceSSL = true;
+      #   locations."/" = {
+      #     proxyPass = "http://127.0.0.1:1115";
+      #     proxyWebsockets = true; # needed if you need to use WebSocket
+      #     extraConfig =
+      #       # required when the target is also TLS server with multiple hosts
+      #       "proxy_ssl_server_name on;"
+      #       +
+      #         # required when the server wants to use HTTP Authentication
+      #         "proxy_pass_header Authorization;";
+      #   };
+      # };
+      # "jp.tlsymposium.com" = {
+      #   sslCertificate = "/etc/ssl/certs/cf.crt";
+      #   sslCertificateKey = "/etc/ssl/private/cf.key";
+      #   forceSSL = true;
+      #   locations."/" = {
+      #     proxyPass = "http://127.0.0.1:10010";
+      #     proxyWebsockets = true; # needed if you need to use WebSocket
+      #     extraConfig =
+      #       # required when the target is also TLS server with multiple hosts
+      #       "proxy_ssl_server_name on;"
+      #       +
+      #         # required when the server wants to use HTTP Authentication
+      #         "proxy_pass_header Authorization;";
+      #   };
+      # };
+      # "sxng.tlsymposium.com" = {
+      #   sslCertificate = "/etc/ssl/certs/cf.crt";
+      #   sslCertificateKey = "/etc/ssl/private/cf.key";
+      #   forceSSL = true;
+      #   locations."/" = {
+      #     proxyPass = "http://127.0.0.1:8787";
+      #     proxyWebsockets = true; # needed if you need to use WebSocket
+      #     extraConfig =
+      #       # required when the target is also TLS server with multiple hosts
+      #       "proxy_ssl_server_name on;"
+      #       +
+      #         # required when the server wants to use HTTP Authentication
+      #         "proxy_pass_header Authorization;";
+      #   };
+      # };
       "gitea.tlsymposium.com" = {
         sslCertificate = "/etc/ssl/certs/cf.crt";
         sslCertificateKey = "/etc/ssl/private/cf.key";
@@ -109,6 +109,21 @@
         forceSSL = true;
         locations."/" = {
           root = "/www/aucsymposium/timer/";
+        };
+      };
+      "auc-research.tlsymposium.com" = {
+        sslCertificate = "/etc/ssl/certs/cf.crt";
+        sslCertificateKey = "/etc/ssl/private/cf.key";
+        forceSSL = true;
+        locations."/" = {
+          proxyPass = "http://127.0.0.1:5522";
+          proxyWebsockets = true; # needed if you need to use WebSocket
+          extraConfig =
+            # required when the target is also TLS server with multiple hosts
+            "proxy_ssl_server_name on;"
+            +
+              # required when the server wants to use HTTP Authentication
+              "proxy_pass_header Authorization;";
         };
       };
     }; # end virtualHosts
