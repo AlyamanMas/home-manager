74dc22c04fcafed529af152241c29f4f121bf377
Alyaman Massarani <alyaman.maasarani@gmail.com>
2025-08-08 01:39:40 +0300

    Re-enable searxng on yvpsh in reverse-proxy module

--- hosts/YVPSH/reverse-proxy.nix
+++ hosts/YVPSH/reverse-proxy.nix
@@ -73,21 +73,21 @@
       #         "proxy_pass_header Authorization;";
       #   };
       # };
-      # "sxng.tlsymposium.com" = {
-      #   sslCertificate = "/etc/ssl/certs/cf.crt";
-      #   sslCertificateKey = "/etc/ssl/private/cf.key";
-      #   forceSSL = true;
-      #   locations."/" = {
-      #     proxyPass = "http://127.0.0.1:8787";
-      #     proxyWebsockets = true; # needed if you need to use WebSocket
-      #     extraConfig =
-      #       # required when the target is also TLS server with multiple hosts
-      #       "proxy_ssl_server_name on;"
-      #       +
-      #         # required when the server wants to use HTTP Authentication
-      #         "proxy_pass_header Authorization;";
-      #   };
-      # };
+      "sxng.tlsymposium.com" = {
+        sslCertificate = "/etc/ssl/certs/cf.crt";
+        sslCertificateKey = "/etc/ssl/private/cf.key";
+        forceSSL = true;
+        locations."/" = {
+          proxyPass = "http://127.0.0.1:8787";
+          proxyWebsockets = true; # needed if you need to use WebSocket
+          extraConfig =
+            # required when the target is also TLS server with multiple hosts
+            "proxy_ssl_server_name on;"
+            +
+              # required when the server wants to use HTTP Authentication
+              "proxy_pass_header Authorization;";
+        };
+      };
       "gitea.tlsymposium.com" = {
         sslCertificate = "/etc/ssl/certs/cf.crt";
         sslCertificateKey = "/etc/ssl/private/cf.key";
