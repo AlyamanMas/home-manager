d3ae8a8443b8bdbfb986f3730beb4d0727e40366
Alyaman Massarani <alyaman.maasarani@gmail.com>
2025-09-27 02:34:52 +0300

    Enable KDE Plasma on YPC

--- hosts/modules/kde.nix
+++ hosts/modules/kde.nix
@@ -12,6 +12,8 @@
     desktopManager.plasma6.enable = true;
     # Needed to run in Wayland mode according to wiki; haven't tested without
     # displayManager.defaultSession = "plasma";
+    # displayManager.sddm.enable = lib.mkForce true;
+    # displayManager.execCmd = lib.mkForce "exec /run/current-system/sw/bin/sddm";
   };
   # Needed for compatibility with GNOME
   programs.ssh.askPassword = pkgs.lib.mkForce "${pkgs.seahorse.out}/libexec/seahorse/ssh-askpass";
