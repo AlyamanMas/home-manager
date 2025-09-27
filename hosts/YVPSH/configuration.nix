f08c4f39e83b612f90bc35edf8432576c26382bf
Alyaman Massarani <alyaman.maasarani@gmail.com>
2025-09-27 02:15:54 +0200

    Move GnuPG from system package to config option in YVPSH config

--- hosts/YVPSH/configuration.nix
+++ hosts/YVPSH/configuration.nix
@@ -52,9 +52,9 @@
     neovim
     btop
     git-crypt
-    pinentry-curses
-    gnupg
   ];
+
+  programs.gnupg.agent.enable = true;
 
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
