136d386b847519125a9f69e2beb5e888e5b36d38
Alyaman Massarani <alyaman.maasarani@gmail.com>
2025-09-27 02:25:04 +0200

    Added explicit pinentry lib.mkForce to YVPSH config

--- hosts/YVPSH/configuration.nix
+++ hosts/YVPSH/configuration.nix
@@ -55,6 +55,7 @@
   ];
 
   programs.gnupg.agent.enable = true;
+  programs.gnupg.agent.settings = {"pinentry-program" = lib.mkForce "${pkgs.pinentry-curses}/bin/pinentry-curses";};
 
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
