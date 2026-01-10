328997a4dee64d18751e69e0311bd8bd05f32a6d
Alyaman Massarani <alyaman.maasarani@gmail.com>
2026-01-10 19:45:57 +0200

    revert commit to refactor flake inputs since apparently flake inputs are dumb and cannot be computed

--- flake.nix
+++ flake.nix
@@ -1,13 +1,45 @@
 {
   inputs = {
+    # basics {{{
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-25.11";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-  }
-  // (import ./extra-flake-inputs.nix);
+    # }}}
+
+    # extra {{{
+    "git-aliases.nu" = {
+      url = "github:KamilKleina/git-aliases.nu";
+      flake = false;
+    };
+    dms = {
+      url = "github:AvengeMedia/DankMaterialShell/stable";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    # TODO: bump to 0.7.0 or above when it becomes stable
+    nix-flatpak.url = "github:gmodena/nix-flatpak/?ref=v0.6.0";
+    # }}}
+
+    # legacy {{{
+    # TODO: remove in favor of flatpak. perhaps do so too with other apps?
+    zen-browser.url = "github:0xc000022070/zen-browser-flake";
+    # TODO: remove; we are using breeze now
+    rose-pine-hyprcursor = {
+      url = "github:ndom91/rose-pine-hyprcursor";
+      inputs.nixpkgs.follows = "nixpkgs";
+      # inputs.hyprlang.follows = "hyprland/hyprlang";
+    };
+    # required for nix-on-droid until new versions are supported
+    # TODO: remove anything related to nix-on-droid, since it seems like we are not going to use it
+    nixpkgs-legacy.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nix-on-droid = {
+      url = "github:nix-community/nix-on-droid/release-24.05";
+      inputs.nixpkgs.follows = "nixpkgs-legacy";
+    };
+    #}}}
+  };
 
   outputs =
     {
@@ -27,7 +59,6 @@
     in
     {
       nixosConfigurations = {
-        # TODO: switch host names and dir names to lowercase
         "YPC2-NIXOS2" = nixpkgs-stable.lib.nixosSystem {
           modules = [ ./hosts/YPC/configuration.nix ];
           specialArgs = {
@@ -53,7 +84,6 @@
       };
 
       homeConfigurations = {
-        # TODO: after updating host names to lowercase, update here to match
         "alyaman@YPC2-NIXOS2" = home-manager.lib.homeManagerConfiguration {
           inherit pkgs;
           modules = [
@@ -91,3 +121,4 @@
       formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixfmt-rfc-style;
     };
 }
+# vim: foldmethod=marker
