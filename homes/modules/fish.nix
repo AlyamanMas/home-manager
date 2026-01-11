3d6987fe99f7b5a322e5b551d26a3545d08f3ca9
Alyaman Massarani <alyaman.maasarani@gmail.com>
2026-01-12 01:52:17 +0200

    use native bass plugin for fish in home module

--- homes/modules/fish.nix
+++ homes/modules/fish.nix
@@ -10,12 +10,7 @@
       {
         # Allows to source bash/shell scripts
         name = "bass";
-        src = pkgs.fetchFromGitHub {
-          owner = "edc";
-          repo = "bass";
-          rev = "79b62958ecf4e87334f24d6743e5766475bcf4d0";
-          sha256 = "0dy53vzzpclw811gxv1kazb8rm7r9dyx56f5ahwd1g38x0pympyx";
-        };
+        inherit (pkgs.fishPlugins.bass) src;
       }
       {
         name = "plugin-git";
