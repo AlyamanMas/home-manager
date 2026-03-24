82edebdfaf2c35a0485c08f75bc352e2cb87ec0a
Alyaman Massarani <alyaman.maasarani@gmail.com>
2026-03-24 17:23:40 +0300

    add vulkan acceleration to ollama if ypc3; add OLLAMA_KEEP_ALIVE=20m

--- hosts/modules/ollama.nix
+++ hosts/modules/ollama.nix
@@ -1,7 +1,24 @@
 {
+  config,
+  ...
+}:
+
+{
   services.ollama = {
     enable = true;
-    acceleration = "cuda";
+    acceleration =
+      let
+        hostname = config.networking.hostName;
+      in
+      if hostname == "ypc2" then
+        "cuda"
+      else if hostname == "ypc3" then
+        "vulkan"
+      else
+        null;
     host = "0.0.0.0";
+    environmentVariables = {
+      OLLAMA_KEEP_ALIVE = "20m";
+    };
   };
 }
